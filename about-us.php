6b70e6ad8bdb8b263eecff1511e4751641d64614
ana marie <anarie092223@gmail.com>
2024-04-22 11:30:27 +0800

    styles changed

--- about-us.php
+++ about-us.php
@@ -18,7 +18,7 @@
          <meta charset="UTF-8">
          <meta name="viewport" content="width=device-width, initial-scale=1.0">
          <title>CONquest: Event Planner</title>
-         <link rel="stylesheet" href="css/styles.css">
+         <link rel="stylesheet" href="css/about-styles.css">
          <link rel="stylesheet" href="css/common-styles.css">
          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
